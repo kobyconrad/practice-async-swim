b41901cc137a9b34b283b8cfac86967c4008fd3b
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-09 13:51:36 -0700

    connected to server and sent a response

--- client/js/httpHandler.js
+++ client/js/httpHandler.js
@@ -1,10 +1,23 @@
-(function() {
-
-  const serverUrl = 'http://127.0.0.1:3000';
+(function () {
+  const serverUrl = "http://127.0.0.1:3000";
 
   //
   // TODO: build the swim command fetcher here
   //
+
+  const fetchAnything = () => {
+    $.ajax({
+      type: "GET",
+      url: serverUrl,
+      success: (res) => {
+        console.log(res);
+      },
+    });
+  };
+
+  $("#target").click(() => {
+    fetchAnything();
+  });
 
   /////////////////////////////////////////////////////////////////////
   // The ajax file uplaoder is provided for your convenience!
@@ -13,37 +26,36 @@
 
   const ajaxFileUplaod = (file) => {
     var formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
     $.ajax({
-      type: 'POST',
+      type: "POST",
       data: formData,
-      url: 'FILL_ME_IN',
+      url: "FILL_ME_IN",
       cache: false,
       contentType: false,
       processData: false,
       success: () => {
         // reload the page
         window.location = window.location.href;
-      }
+      },
     });
   };
 
-  $('form').on('submit', function(e) {
+  $("form").on("submit", function (e) {
     e.preventDefault();
 
-    var form = $('form .file')[0];
+    var form = $("form .file")[0];
     if (form.files.length === 0) {
-      console.log('No file selected!');
+      console.log("No file selected!");
       return;
     }
 
     var file = form.files[0];
-    if (file.type !== 'image/jpeg') {
-      console.log('Not a jpg file!');
+    if (file.type !== "image/jpeg") {
+      console.log("Not a jpg file!");
       return;
     }
 
     ajaxFileUplaod(file);
   });
-
 })();
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const headers = require('./cors');
-const multipart = require('./multipartUtils');
+const fs = require("fs");
+const path = require("path");
+const headers = require("./cors");
+const multipart = require("./multipartUtils");
 
 // Path for the background image ///////////////////////
-module.exports.backgroundImageFile = path.join('.', 'background.jpg');
+module.exports.backgroundImageFile = path.join(".", "background.jpg");
 ////////////////////////////////////////////////////////
 
 let messageQueue = null;
@@ -12,9 +12,13 @@ module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
-module.exports.router = (req, res, next = ()=>{}) => {
-  console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
-  res.end();
+module.exports.router = (req, res, next = () => {}) => {
+  console.log("Serving request type " + req.method + " for url " + req.url);
+
+  if (req.url === "/") {
+    res.writeHead(200, headers);
+    res.end("whoa dude this is the end");
+  }
+
   next(); // invoke next() at the end of a request to help with testing!
 };
